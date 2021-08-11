26957b8d9558cea5e4fa5f85d82c184d4838b684
MuhammadSiddik12 <siddik20@navgurukul.org>
2021-08-11 19:58:36 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const accountSid = process.env.ACCOUNT_SID
+const authToken = process.env.AUTH_TOKEN
+const client = require('twilio')(accountSid, authToken)
+const app = require('express')()
+
+app.get('/', (req, res) => {
+    res.send({ message: "Hello World" })
+})
+
+
+app.get('/sendMessage', (req, res) => {
+    sendMessage()
+    setTimeout(() => {
+        res.send({ message: "Message sent successFully" })
+    }, 3000)
+
+})
+
+function sendMessage() {
+    client.messages
+        .create({
+            body: "This is a test message 2",
+            from: "+15053081832",
+            to: "+919811073783"
+        }).then((message) => console.log(message))
+        .catch((err) => console.log(err))
+}
+
+app.listen(process.env.PORT, () => console.log(`Server is Running on port ${process.env.PORT}`))
